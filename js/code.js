f84a821b6f4762705c5a880b330d4f0bed81905c
Mu-een <mueenslam@gmail.com>
2022-11-07 15:39:25 +0200

    js

---
+++ js/code.js
@@ -0,0 +1,16 @@
+function myFunction () {
+    alert ("Hello! My name is Mu-een!!")
+}
+
+let firstName ='Mu-een';
+let userAge = 23;
+let male = true;
+
+document.write("<br>" + firstName);
+document.write("<br>" + userAge);
+document.write("<br>" + male + "<br>");
+
+document.write(`Name: ${firstName}<br>Age: ${userAge}<br>Male: ${male}`)
+
+let names = ['Daniel', 'Mishaine', 'Matthew'];
+console.log(names);
